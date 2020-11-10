a05fc8922ac35715a59a4573675f13e1d060d631
Mary <mary@promptlink.com>
2020-11-10 21:29:45 +0200

    Initial Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Mary on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
