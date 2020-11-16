d1993d49985894512c55bda7baad694275926530
Mary <mary@promptlink.com>
2020-11-16 16:32:06 +0200

    Initial commit

--- GitHubSearch/ViewController.swift
+++ GitHubSearch/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import Alamofire
+import Foundation
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchField: UITextField!
+    
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var resultsTable: UITableView!
+    
+    
+    let tableHelper: SearchTableViewController = SearchTableViewController()
+    let requestQueue1 = DispatchQueue(label: "queue1")
+    let requestQueue2 = DispatchQueue(label: "queue2")
+    
     override func viewDidLoad() {
+        tableHelper.zzz = self
+        resultsTable.dataSource = tableHelper
+        resultsTable.delegate = tableHelper
+        var searchText = searchField.text
+        resultsTable.allowsSelection = true
+        resultsTable.isScrollEnabled = true
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
 
+    func alamofireResponse() {
+        let q = searchField.text!
+        let url = "https://api.github.com/search/repositories?q=" + "\(q)" + "&sort=stars&order=desc"
+      
+        AF.request(url).responseJSON(completionHandler: { response in
+            switch response.result {
+                        case .success(let JSON):
+                            let response = JSON as! NSDictionary
+                            let name = response.value(forKey: "items")
+                          //  print(name is NSArray)
+                         //   print(name)
+                            
+                            if let array = name as? [[String: Any]] {
+                                let reposArray = array.compactMap { $0["full_name"] as? String }
+                           print(reposArray)
+                            
+                            }
 
+                        case .failure(let error):
+                            print("Error!!! \(error)")
+                        }
+        })
+
+    }
+
+    
+    @IBAction func doSearch(_ sender: Any) {
+alamofireResponse()
 }
 
+    
+    
+}
